0314aac61012c896e4ecfe31eb3deb75a5523042
Shrey Patel <shrey2411@gmail.com>
2020-02-22 21:32:46 -0800

    database operative

--- backend/api/routes/home.js
+++ backend/api/routes/home.js
@@ -1,17 +1,23 @@
 const express = require('express');
 const router = express.Router();
+const db = require('../../models');
 
 
-router.get('/', (req, res, next) => {
-    res.status(200).json({
-        message: "Handle GET req to /home",
-    });
+db.sequelize.sync();
+
+router.get('/todo', (req, res, next) => {
+    db.Todo.findAll().then((todo) => res.send(todo));
+    // res.status(200).json({
+    //     message: "Handle GET req to /home",
+    // });
 });
 
-router.post('/', (req, res, next) => {
-    res.status(200).json({
-        message: "Handle POST req to /home",
-    });
+router.post('/todo', (req, res, next) => {
+    console.log(req.body);
+    db.Todo.create({
+        text: req.body.text,
+    }).then(submitedTodo => res.send(submitedTodo))
+        .catch((err) => res.send("error, you sent: " + err));
 });
 
 module.exports = router;
--- backend/index.js
+++ backend/index.js
@@ -9,10 +9,7 @@ const cookieParser = require('cookie-parser');
 const path = require('path');
 
 const homeRoutes = require('./api/routes/home');
-app.use('/home', homeRoutes);
 
-app.set('view engine', 'ejs'); // set the view engine to ejs
-app.set('views', './views'); // set the directory of views
 app.use(express.static(path.join(__dirname, '/public'))); // specify the path of static directory
 
 // use body parser to parse JSON and urlencoded request bodies
@@ -26,6 +23,17 @@ app.use(session({
   resave: false,
   saveUninitialized: true,
 }));
+//Allow Access Control
+app.use(function(req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT,DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers');
+  res.setHeader('Cache-Control', 'no-cache');
+  next();
+});
+
+app.use('/home', homeRoutes);
 
 const server = app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`);
