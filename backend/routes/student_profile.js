b0f5a0d18389343c29a8aafbb3d2d4df8c0c0f3d
Shrey Patel <shrey2411@gmail.com>
2020-03-02 21:45:39 -0800

    student profile APIs added

--- backend/routes/student_profile.js
+++ backend/routes/student_profile.js
@@ -8,8 +8,9 @@ let selectStudentSql = 'SELECT * FROM student_profile WHERE user_id = ?;';
 let eduSql = 'SELECT * FROM student_education WHERE user_id = ?;';
 let expSql = 'SELECT * FROM student_experience WHERE user_id = ?;';
 
+
 //> /student-profile/:user_id
-router.get('/:user_id', checkAuth, (req, res) => {
+router.get('/:user_id', (req, res) => {
     // console.log(req.body);
     const user_id = req.params.user_id;
     
@@ -34,25 +35,74 @@ router.get('/:user_id', checkAuth, (req, res) => {
         });    
 });
 
+router.put('/', checkAuth, (req, res) => {
+    const user_id = req.jwtData.user_id;
+    const data = req.body;
+    let updateProfileSql = 'UPDATE student_profile SET ? WHERE user_id = ?;';
+    db.query(updateProfileSql, [data, user_id], (err, result) => {
+        if(err) res.status(500).send(err);
+        else {
+            res.status(200).json({ msg: 'success', result: result });
+        }
+    });
+});
+
+// education section
 router.post('/education', checkAuth, (req, res) => {
-    //  TODO check same user session
-    console.log(req.jwtData);
-    res.sendStatus(200);
-    
-    // const { college_name, degree, major, start_date, end_date, gpa, user_id } = req.body;
-    // const eduData = { college_name, degree, major, start_date, end_date, gpa, user_id };
-    // let InsertEduSql = ' INSERT INTO student_education SET ?;';
-    // db.query(InsertEduSql, eduData, (err, result) => {
-
-    // });
-
+    const { college_name, degree, major, start_date, end_date, gpa } = req.body;
+    const { user_id } = req.jwtData;
+    const eduData = { user_id, college_name, degree, major, start_date, end_date, gpa };
+    let InsertEduSql = ' INSERT INTO student_education SET ?;';
+    db.query(InsertEduSql, eduData, (err, result) => {
+        if(err) res.status(500).send(err);
+        else {
+            res.status(200).json({ msg: 'success', id: result['insertId'] });
+        }
+    });
 });
 
-router.put('/education/:id', (req, res) => {
+router.put('/education/:id', checkAuth, (req, res) => {
     //  TODO check same user session
-    console.log('updating education for id : ', req.params);
-    res.send(`updating education for id : ${req.params}`);
+    let updateEduSql = 'UPDATE student_education SET ? WHERE id = ? AND user_id = ?;';
+    const data = req.body;
+    const id = req.params.id;
+    console.log(`update data for user id: ${id}`);
+    console.log('data: ', data);    
+    db.query(updateEduSql, [data, id, req.jwtData.user_id], (err, result) => {
+        if(err) res.status(500).send(err);
+        else {
+            res.status(200).json({ msg: 'success', result: result });
+        }
+    });
 });
 
+// experience section
+router.post('/experience', checkAuth, (req, res) => {
+    const { company_name, title, location, start_date, end_date, work_desc } = req.body;
+    const { user_id } = req.jwtData;
+    const data = { user_id, company_name, title, location, start_date, end_date, work_desc };
+    let InsertEduSql = ' INSERT INTO student_experience SET ?;';
+    db.query(InsertEduSql, data, (err, result) => {
+        if(err) res.status(500).send(err);
+        else {
+            res.status(200).json({ msg: 'success', id: result['insertId'] });
+        }
+    });
+});
+
+router.put('/experience/:id', checkAuth, (req, res) => {
+    //  TODO check same user session
+    updateEduSql = 'UPDATE student_experience SET ? WHERE id = ? AND user_id = ?;';
+    const data = req.body;
+    const id = req.params.id;
+    console.log(`update data for user id: ${id}`);
+    console.log('data: ', data);    
+    db.query(updateEduSql, [data, id, req.jwtData.user_id], (err, result) => {
+        if(err) res.status(500).send(err);
+        else {
+            res.status(200).json({ msg: 'success', result: result });
+        }
+    });
+});
 
 module.exports = router;
